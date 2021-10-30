4502fd6a8bc7c4cf49cad49d7073254fa1ac3db2
wraith-0 <19ucs254@lnmiit.ac.in>
2021-10-30 17:45:27 +0530

    schema update

--- database.sql
+++ database.sql
@@ -1,33 +1,42 @@
-create table login(
-    usertype varchar(100),
-    userId varchar(100) PRIMARY KEY,
-    passw varchar(100)
-);
-
 create table admin(
-    userId varchar(100) PRIMARY KEY,
-    name varchar(100),
-    DOB Date
+    userId varchar(255) PRIMARY KEY,
+    name varchar(255),
+    DOB Date,
+    passw varchar(255)
 );
 
 create table student(
-    userId varchar(100) PRIMARY KEY,
-    name varchar(100),
-    joining_year varchar(100),
+    userId varchar(255) PRIMARY KEY,
+    name varchar(255),
+    joining_year varchar(255),
     Student_DOB Date,
-    Branch varchar(100)
+    Branch varchar(255),
+    passw varchar(255)
 );
 
 create table course(
-    course_id varchar(100) PRIMARY KEY,
-    coursename varchar(100),
-    type varchar(100),
-    credits INT
+    course_id varchar(255) PRIMARY KEY,
+    coursename varchar(255) UNIQUE 
+);
+
+create table courseDetail(
+    coursename varchar(255)  PRIMARY KEY,
+    type varchar(255),
+    credits INT,
+	FOREIGN KEY (coursename) REFERENCES course(coursename)
+);
+
+create table courseAvailibilty(
+    course_id varchar(255),
+    semester INT,
+    branch varchar(255),
+    FOREIGN KEY (course_id) REFERENCES course(course_id),
+    PRIMARY KEY(course_id, semester, branch)
 );
 
 create table courseEnrollment(
-    course_id varchar(100) ,
-    student_id varchar(100) ,
+    course_id varchar(255) ,
+    student_id varchar(255) ,
     dateofEnrollment Date, FOREIGN KEY (student_id) REFERENCES student(userId), 
     FOREIGN KEY (course_id) REFERENCES course(course_id),
     PRIMARY KEY(course_id,student_id)
